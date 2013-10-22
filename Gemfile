1260d28d3ada502dc7b542440ee39c8a2193e776
Apprentice <apprentice@dbc14.local>
2013-10-21 23:27:27 -0700

    finish facebook omniauth spike (ken and dan)

--- Gemfile
+++ Gemfile
@@ -1,9 +1,15 @@
 
 source 'https://rubygems.org'
-ruby "2.0.0"
+ruby "1.9.3"
 
 gem 'sinatra'
 gem 'sinatra-activerecord'
-gem 'rspec'
-gem "shotgun"
+gem 'omniauth-facebook'
 
+
+group :development, :test do
+  gem 'rspec'
+  gem "shotgun"
+  gem 'debugger'
+  gem 'dotenv'
+end
--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
-require 'sinatra'
+%w(omniauth omniauth-facebook sinatra dotenv debugger).each { |dependency| require dependency} 
+
+Dotenv.load
+
+configure do
+  enable :sessions
+  
+  use OmniAuth::Builder do 
+    provider :facebook, ENV['APP_ID'], ENV['APP_SECRET']
+  end
+end
 
 get '/' do
   erb :index
 end
+
+
+get '/auth/facebook/callback' do
+  env['omniauth.auth']
+  debugger
+end
+
+# env['omniauth.auth']['info'][:email]
+# env['omniauth.auth']['info']['first_name']
+# env['omniauth.auth']['info']['name']
+# env['omniauth.auth']['info']['image']
