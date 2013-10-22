8af226372502452bdff98beae4f4ebbaf4b77ba7
carter <carter.sowers@gmail.com>
2013-10-21 18:47:12 -0700

    added Gemfile, spec_helper.rb, user_spec.rb

---
+++ user_spec.rb
@@ -0,0 +1,13 @@
+describe User do
+  context "validations" do
+    it { should validate_presence_of :name }
+  end
+
+  context "associations" do
+    xit { pending }
+  end
+
+  context "#name" do
+    xit { pending }
+  end
+end
