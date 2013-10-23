25dbd2adf24302cb12845d212a5f7782b5c28e31
Apprentice <apprentice@dbc15.local>
2013-10-22 19:57:49 -0700

    facebook authenticates new user

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
-
+require 'omniauth'
+require 'omniauth-facebook'
 require 'sinatra'
+require 'dotenv'
+require 'debugger'
+
+Dotenv.load
+
+configure do 
+  enable :sessions
+
+  use OmniAuth::Builder do 
+    provider :facebook, ENV['APP_ID'], ENV['APP_SECRET']
+  end
+end
 
 get '/' do
   erb :index
+end
+
+get '/auth/facebook/callback' do
+  env['omniauth.auth']
+  "Hello World"
 end
